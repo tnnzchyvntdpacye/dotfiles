cff4c7872c7a05a9d1865ba4ef5807f099f750ea
simon <simonchik00@bk.ru>
2023-05-15 19:24:57 +0000

    dd .git line

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 ln -sf ~/git/dotfiles/.aliases         ~/.aliases
 ln -sf ~/git/dotfiles/.bash_profile    ~/.bash_profile
 ln -sf ~/git/dotfiles/.bashrc          ~/.bashrc
-ln -sf ~/git/dotfiles/.git             ~/.git
 ln -sf ~/git/dotfiles/.inputrc         ~/.inputrc
 ln -sf ~/git/dotfiles/.tmux.conf       ~/.tmux.conf
 ln -sf ~/git/dotfiles/.vimrc           ~/.vimrc
